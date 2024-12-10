9a7b8ffe637fee8a6dbb114153a75555cdb171f8
redicee <55047851+redicee@users.noreply.github.com>
2024-12-11 04:10:51 +0800

    updated to use line v3 sdk

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import requests
 import json
-# 載入 LINE Message API 相關函式庫
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from waitress import serve
 
+from linebot.v3 import WebhookParser
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
 app = Flask(__name__)
 
 
@@ -20,69 +31,80 @@ SECRET = 'Replace with channel secret.'
 
 base_url = "http://localhost:3001/api/v1/"
 
+configuration = Configuration(access_token = ACCESS_TOKEN)
+handler = WebhookParser(SECRET)
 
 @app.route("/", methods=['POST','GET'])
 def linebot():
-    body = request.get_data(as_text=True)                   # 取得收到的訊息內容
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+
     try:
-        json_data = json.loads(body)                        # json 格式化訊息內容
-        line_bot_api = LineBotApi(ACCESS_TOKEN)             # 確認 token 是否正確
-        handler = WebhookHandler(SECRET)                    # 確認 secret 是否正確
-        signature = request.headers['X-Line-Signature']     # 加入回傳的 headers
-        handler.handle(body, signature)                     # 綁定訊息回傳的相關資訊
-        event = json_data.get('events',[{}])[0]
-        reply_token = event.get('replyToken')               # 取得回傳訊息的 Token
-        message = event.get('message', {})                  
-        msg_type = message.get('type')                      # 取得 LINE 收到的訊息類型
+        try:
+            signature = request.headers['X-Line-Signature']    
+            events = handler.parse(body, signature)        
+        except InvalidSignatureError:
+            abort(400)
 
-
-        if msg_type=='text':
-            username = event.get('source',{}).get('userId')
-            user_message = message.get('text')
-            try:
-                profile = line_bot_api.get_profile(str(username))
-                disp_name_str = str(profile)
-                disp_name_json = json.loads(disp_name_str)                
-                disp_name = disp_name_json['displayName']
-                print(f"Disp name:{disp_name}")
-            except LineBotApiError as e:
-                print(e)
-            
-            payload = {
-                "message": user_message,
-                "mode": "chat",
-                "attachments": []
-            }
-            headers = {
-                "Authorization": f"Bearer {API_KEY}",
-                "Content-Type": "application/json"
-            }           
-            thread_template = {
-                "name": disp_name,
-                "slug": username,
-            } 
-
-            print("Checking User..")
-            api_call("check_user", headers=headers, user=thread_template)                        
-            try:
-                print("Chatting...")
-                response = api_call("thread_chat", headers=headers, payload=payload, user=thread_template)                        
-            except Exception as e:
-                app.logger.error("An error occurred: %s", e)
-                return "An error occurred, check logs.", 500    
-            print(response.status_code)
-            response_data = response.json()
-            print(response_data)
-            reply = response_data["textResponse"]
-
-            
-        else:
-            reply = 'Input is not text.'
-        line_bot_api.reply_message(reply_token,TextSendMessage(reply)) # 回傳訊息
-    except:
-        print(body)                                          # 如果發生錯誤，印出收到的內容
-    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+        body_event = json_data.get('events',[{}])[0]
+        username = body_event.get('source',{}).get('userId')
         
+        for event in events:
+            if not isinstance(event, MessageEvent):
+                continue
+            if not isinstance(event.message, TextMessageContent):
+                continue
+            with ApiClient(configuration) as api_client:
+                msg_type = event.message.type  
+                line_bot_api = MessagingApi(api_client)
+                try:
+                    profile = line_bot_api.get_profile(username)
+                    try:
+                        disp_name = profile.display_name
+                    except:
+                        print("Failed to get display name")
+                except:
+                    print("Failed to get profile")
+                if msg_type=='text':
+                    message = event.message
+                    print(f'{disp_name} asked the question "{message.text}"')
+                    payload = {
+                        "message": message.text,
+                        "mode": "chat",
+                        "attachments": []
+                    }
+                    headers = {
+                        "Authorization": f"Bearer {API_KEY}",
+                        "Content-Type": "application/json"
+                    }           
+                    thread_template = {
+                        "name": disp_name,
+                        "slug": username,
+                    } 
+                    api_call("check_user", headers=headers, user=thread_template)                        
+                    try:
+                        print("Generating reply...")
+                        response = api_call("thread_chat", headers=headers, payload=payload, user=thread_template)                        
+                    except Exception as e:
+                        app.logger.error("An error occurred: %s", e)
+                        return "An error occurred, check logs.", 500
+                
+                    response_data = response.json()
+                    reply = response_data["textResponse"]
+                    line_bot_api.reply_message_with_http_info(
+                        ReplyMessageRequest(
+                            reply_token=event.reply_token,
+                            messages=[TextMessage(text=reply)]
+                        )
+                    )
+                    print("Reply sent.")
+                else:
+                    print('Not Text.')        
+    except:
+        print("Exception.")                                         
+    return 'OK'                                                      
+        
+
 def api_call(METHOD, headers=None, payload=None, user=None):
     
     API_ENDPOINTS = {
@@ -99,7 +121,7 @@ def api_call(METHOD, headers=None, payload=None, user=None):
         elif METHOD == "thread_chat":
             response = requests.post(url, json=payload, headers=headers)
         elif METHOD == "check_user":
-            print("Checking function")
+            print("Checking User..")
             response = requests.get(url, headers=headers)
             check_user_threads(response.json(), user, headers)
         else:
@@ -121,5 +143,7 @@ def check_user_threads(response_data, user, headers):
     
 
 if __name__ == "__main__":
+    print("Server starting. Default port is 3535")
     # app.run(debug=True)   # Uncomment this to use flask
     serve(app, host="127.0.0.1", port=3535)  #for waitress
+    
